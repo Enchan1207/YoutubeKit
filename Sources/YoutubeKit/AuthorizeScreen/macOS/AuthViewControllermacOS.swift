bd0f7fd40337793e3c4e10c3a0d75c4747f8e57e
Enchan <enchant0130@gmail.com>
2021-04-03 21:25:53 +0900

    [Update #4] add demo appkication for mac

---
+++ Sources/YoutubeKit/AuthorizeScreen/macOS/AuthViewControllermacOS.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewController.swift
+//  YKDemoiOS
+//
+//  Created by EnchantCode on 2021/04/03.
+//
+
+#if os(OSX)
+
+import Cocoa
+
+class AuthViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ん?")
+    }
+}
+
+#endif
---
+++ Sources/YoutubeKit/AuthorizeScreen/macOS/AuthWindow.swift
@@ -0,0 +1,8 @@
+//
+//  AuthWindow.swift
+//  YKDemomacOS
+//
+//  Created by EnchantCode on 2021/04/03.
+//
+
+import Foundation
