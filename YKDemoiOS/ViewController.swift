5e2140a7c8713a026d1bf8f3253881db32dd8c66
Enchan <enchant0130@gmail.com>
2021-04-03 20:41:12 +0900

    [Update #4] authorize demo

--- YKDemoiOS/ViewController.swift
+++ YKDemoiOS/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 import YoutubeKit
 
 class ViewController: UIViewController {
+    
+    let youtube = YoutubeKit(apiCredential: API_CREDENTIAL, accessCredential: ACCESS_CREDENTIAL)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let scope: [YoutubeKit.Scope] = [.readwrite, .forceSSL, .audit, .upload]
+        self.youtube.authorize(presentViewController: self, scope: scope) { (credential) in
+            print(credential)
+        } failure: { (error) in
+            print(error)
+        }
+    }
 }
 
