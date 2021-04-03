0cd78c5c5bcb2eaf721059097b02685aaec9ae8c
Enchan <enchant0130@gmail.com>
2021-04-03 20:17:41 +0900

    [Update #4] add Demo application for iOS

---
+++ YKDemoiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YKDemoiOS
+//
+//  Created by EnchantCode on 2021/04/03.
+//
+
+import UIKit
+import YoutubeKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
